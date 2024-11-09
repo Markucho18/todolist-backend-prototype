af4d76fab0f85b0a5cbdb33fd97e5aaa17e1d2bf
Markucho18 <marcossosa21415@gmail.com>
2024-11-09 10:08:49 -0300

    Componente tareas entre comillas, completar tareas funcional y tal.

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -20,14 +20,22 @@ router.get("/", async (req, res)=>{
   }
 })
 
-router.get("/inbox", async(req, res) => {
+router.get("/no-date", async(req, res) => {
   const userId = req.user.id
-  
+  res.send("Saca las panochass")
 })
 
 router.get("/today", async(req, res) => {
   const userId = req.user.id
-  
+  const query = "SELECT * FROM tasks WHERE task_deadline = CURDATE() AND user_id = ?"
+  try{
+    const [rows] = await pool.query(query, [userId])
+    if(rows.length === 0) return res.status(204).json({message: "No tasks found today"})
+    res.render("tasksToday", {tasks: rows})
+  } catch(error){
+    console.log({error})
+    res.json({message: "Error has ocurred"})
+  }
 })
 
 router.get("/tomorrow", async(req, res) => {
@@ -63,13 +71,27 @@ router.post("/", async(req, res)=>{
   }
 })
 
-router.put("/:id", (req, res) => {
+router.put("/complete/:id", async (req, res) => {
+  const userId = req.user.id
+  const taskId = req.params.id
+  const query = "UPDATE tasks SET task_completed = NOT task_completed WHERE task_id = ? AND user_id = ?"
+  try{
+    const result = await pool.query(query, [taskId, userId])
+    if(result[0].affectedRows === 0) return res.status(404).json({message: "Task or user not found"})
+    res.status(200).json({message: "Task completed succcessfully"})
+  } catch(error){
+    console.log({error})
+    res.status(500).json({message: "Server error has ocurred"})
+  }
+})
+
+router.put("/edit/:id", (req, res) => {
   const userId = req.user.id
   const taskId = req.params.id
   const {task_title, task_deadline, task_priority, task_description} = req.body
   const query = `
-    UPDATE tasks SET (user_id, task_title, task_deadline, task_priority, task_description)
-    VALUES (?, ?, ?, ?, ?) WHERE task_id = ?
+    UPDATE tasks SET (task_title, task_deadline, task_priority, task_description)
+    VALUES (?, ?, ?, ?) WHERE task_id = ? AND user_id = ?
   `
 })
 
